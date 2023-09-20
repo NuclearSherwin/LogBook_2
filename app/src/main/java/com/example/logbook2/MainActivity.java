fa6d064981375177bb2a94c7b70aa81dea2ae5b5
NuclearSherwin <phongspacenasa@gmail.com>
2023-09-20 23:38:41 +0900

    initial values

--- app/src/main/java/com/example/logbook2/MainActivity.java
+++ app/src/main/java/com/example/logbook2/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.logbook2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private int currentIndex = 0;
+    private int[] imageIds = {R.drawable.image_1, R.drawable.image_2, R.drawable.image_3};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
